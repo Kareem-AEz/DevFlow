e23639dae8d627b54d9ec6c81a7a409f4cecac10
Kareem-AEz <kemoahmedahmedkemo@gmail.com>
2025-05-12 11:28:26 +0300

    initial commit - navbar and mode toggle

---
+++ src/app/api/db.ts
@@ -0,0 +1,9 @@
+const books = [
+	{ id: 1, name: "How to win friends and influence people" },
+	{ id: 2, name: "Atomic habits" },
+	{ id: 3, name: "The 5 AM Club" },
+	{ id: 4, name: "The 4 hour work week" },
+	{ id: 5, name: "The 7 habits of highly effective people" },
+];
+
+export default books;
