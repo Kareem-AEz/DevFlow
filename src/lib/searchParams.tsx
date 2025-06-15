1968f2b13aa7e1627efec5b96cbaab0be2ef5a01
Kareem-AEz <kareem.ahmedez@gmail.com>
2025-06-16 00:10:11 +0300

    ✨ Transform README and DOCS

--- src/lib/searchParams.tsx
+++
@@ -1,7 +0,0 @@
-import { createLoader, parseAsString } from "nuqs/server";
-
-export const searchParams = {
-  query: parseAsString,
-};
-
-export const getSearchParams = createLoader(searchParams);
