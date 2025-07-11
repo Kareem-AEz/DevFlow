f420a8b5bc7ad0949cb04ff2f6fd6d6df1c15212
Kareem-AEz <kareem.ahmedez@gmail.com>
2025-07-11 17:24:22 +0300

    Implement Log Out and Show UserInfo on Home

--- src/components/layout/navigation/LeftSidebar.tsx
+++ src/components/layout/navigation/LeftSidebar.tsx
@@ -10,11 +10,12 @@ import NavLinks from "./navbar/NavLinks";
 
 async function LeftSidebar() {
   const session = await auth();
+  const userId = session?.user?.id;
 
   return (
     <aside className="background-light900_dark200 no-scrollbar light-border sticky top-0 flex h-screen w-full max-w-72 flex-col justify-between overflow-y-auto p-6 px-5 pt-36 max-lg:max-w-24 max-lg:items-center max-sm:hidden">
       <div>
-        <NavLinks />
+        <NavLinks userId={userId} />
       </div>
 
       {!session && <AuthButtons />}
