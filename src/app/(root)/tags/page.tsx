d8050fa90f6b414a09bdf981fe7c3f1c7f5cf924
Kareem-AEz <kemoahmedahmedkemo@gmail.com>
2025-07-14 16:40:36 +0300

    Implement getTags server action

--- src/app/(root)/tags/page.tsx
+++ src/app/(root)/tags/page.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
-function Tags() {
+import { getTags } from "@/lib/actions/tag.action";
+
+async function Tags() {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "",
+    filter: "",
+    sortBy: "",
+  });
+
+  const { tags } = data || {};
+
+  console.log("TAGS", JSON.stringify(tags, null, 2));
+
   return <div>Tags</div>;
 }
 
