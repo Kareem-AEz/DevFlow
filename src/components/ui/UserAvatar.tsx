751eae93d1734dd40eb830563c076d1fef944e0e
Kareem-AEz <kareem.ahmedez@gmail.com>
2025-08-11 21:02:02 +0300

    fix user avatar image

--- src/components/ui/UserAvatar.tsx
+++ src/components/ui/UserAvatar.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { ROUTES } from "@/constants/routes";
 
@@ -15,7 +14,6 @@ interface Props {
   imageUrl?: string | null;
   className?: string;
   fallbackClassName?: string;
-  size?: number;
 }
 
 const UserAvatar = ({
@@ -24,7 +22,6 @@ const UserAvatar = ({
   imageUrl,
   className = "h-9 w-9",
   fallbackClassName,
-  size = 36,
 }: Props) => {
   const initials = name
     .split(" ")
@@ -36,25 +33,21 @@ const UserAvatar = ({
   return (
     <Link href={ROUTES.PROFILE(userId)}>
       <Avatar className={cn("relative", className)}>
-        {imageUrl ? (
-          <Image
+        {imageUrl && (
+          <AvatarImage
             src={imageUrl}
             alt={name}
             className="w-full object-cover"
-            width={size}
-            height={size}
-            quality={100}
           />
-        ) : (
-          <AvatarFallback
-            className={cn(
-              "primary-gradient font-space-grotesk font-bold tracking-wider text-white",
-              fallbackClassName,
-            )}
-          >
-            {initials}
-          </AvatarFallback>
         )}
+        <AvatarFallback
+          className={cn(
+            "primary-gradient font-space-grotesk font-bold tracking-wider text-white",
+            fallbackClassName,
+          )}
+        >
+          {initials}
+        </AvatarFallback>
       </Avatar>
     </Link>
   );
